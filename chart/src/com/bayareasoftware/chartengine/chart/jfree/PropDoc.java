f38eb7ee52859bec2283731cda26125c7e7fec1b
dave.brown <dave.brown@gmail.com>
2010-08-28 17:52:26 +0000

    Flesh out documenting *all* the properties we understand and support, for all chart types.  Need to fixup the formatting somewhat, and then I will get this onto the google code wiki, and in the demo app, under "Property Reference"

--- chart/src/com/bayareasoftware/chartengine/chart/jfree/PropDoc.java
+++ chart/src/com/bayareasoftware/chartengine/chart/jfree/PropDoc.java
@@ -15,7 +15,11 @@
  */
 package com.bayareasoftware.chartengine.chart.jfree;
 
+import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.bayareasoftware.chartengine.model.types.ChartBeanInfo;
 import com.bayareasoftware.chartengine.model.types.ChartTypeSystem;
@@ -25,32 +29,145 @@ public class PropDoc {
 
     private ChartTypeSystem ts;
     private PrintStream out;
+    private boolean isWiki = false;
+    
     public PropDoc(String file, PrintStream out) throws Exception {
         ts = TypeInspector.load(file);
-        this.out = System.out;
+        this.out = out;
     }
     public void run() throws Exception {
-        ChartBeanInfo cbi = ts.getChartBean();
-        out.println("<table>\n<tr><th>Name</th><th>Type</th><th>Default</th></tr>");
-        printBean(cbi, "chart");
+        List<ChartBeanInfo> l;
+        ChartBeanInfo cbi;
+        out.println("<ul>"); // master list
+        printLi("chart", "chart");
+        printLi("title", "title");
+        printLi("legend", "legend");
+        printLi("marker", "marker");
+
+        printLi("plot", "Plot Types");
+        out.println("<ul>");
+        l = ts.getPlots();
+        for (ChartBeanInfo c : l)
+            if (supportedPlots.get(c.getDisplayname()) != null)
+                printLi(c.getClassname(), c.getDisplayname());
+        out.println("</ul>");
+
+        printLi("axis", "Axis Types");
+        out.println("<ul>");
+        for (String a : AXIS_TYPES.keySet()) {
+            printLi(a, a);
+        }
+        out.println("</ul>");
+        
+        printLi("xyrenderers", "XY/timeseries Graph Types");
+        out.println("<ul>");
+        l = ts.getConcreteXYRenderers();
+        for (ChartBeanInfo c : l) {
+            printLi(c.getClassname(), c.getDisplayname());
+        }
+        out.println("</ul>");
+
+        printLi("categoryrenderers", "Category Graph Types");
+        out.println("<ul>");
+        l = ts.getConcreteCategoryRenderers();
+        for (ChartBeanInfo c : l) {
+            printLi(c.getClassname(), c.getDisplayname());
+        }
+        out.println("</ul>");
+
+        
+        out.println("</ul>"); // master list
+        
+        cbi = ts.getChartBean();
+        printMajorAnchor("chart", "chart");
+        printBean(cbi);  // chart
+        /*
         cbi = ts.getPlotInfo("org.jfree.chart.plot.XYPlot");
-        printBean(cbi, "plot");
+        printBean(cbi);
         cbi = ts.getXYRendererInfo("Line And Shape");
-        printBean(cbi, "renderer");
+        printBean(cbi);
         cbi = ts.getAxis("DateAxis");
-        printBean(cbi, "domain-axis");
+        printBean(cbi);
         cbi = ts.getAxis("NumberAxis");
-        printBean(cbi, "range-axis-X");
+        printBean(cbi);
+        */
+        printMajorAnchor("title", "title");
         cbi = ts.getTextTitleBean();
-        printBean(cbi, "title");
-        out.println("</table>");
+        printBean(cbi);
+
+        cbi = ts.getLegendBean();
+        printMajorAnchor("legend", "legend");
+        printBean(cbi);
+
+        cbi = ts.getMarkerBean();
+        printMajorAnchor("marker", "marker");
+        printBean(cbi);
+
+        printMajorAnchor("plot", "plot");
+        l = ts.getPlots();
+        for (ChartBeanInfo c : l) {
+            if (supportedPlots.get(c.getDisplayname()) != null) {
+                printMinorAnchor(c.getClassname(), c.getDisplayname());
+                printBean(c);
+            }
+        }
+
+        printMajorAnchor("axis", "Axis Types");
+        for (String a : AXIS_TYPES.keySet()) {
+            String displayName = AXIS_TYPES.get(a);
+            cbi = ts.getAxis(displayName);
+            printMinorAnchor(a, displayName);
+            printBean(cbi);
+        }
+
+        printMajorAnchor("xyrenderers", "XY Graph Types");
+        l = ts.getConcreteXYRenderers();
+        for (ChartBeanInfo c : l) {
+            c = ts.getXYRendererInfo(c.getDisplayname());
+            printMinorAnchor(c.getClassname(), c.getDisplayname());
+            printBean(c);
+        }
+
+        printMajorAnchor("categoryrenderers", "Category Graph Types");
+        l = ts.getConcreteCategoryRenderers();
+        for (ChartBeanInfo c : l) {
+            c = ts.getCategoryRendererInfo(c.getDisplayname());
+            printMinorAnchor(c.getClassname(), c.getDisplayname());
+            printBean(c);
+        }
+        
     }
     public static void main(String[] args) throws Exception {
-        new PropDoc("/jfreechart-types.xml", System.out).run();
+        PrintStream out = System.out;
+        if (args.length > 0)
+            out = new PrintStream(new FileOutputStream(args[0]));
+        new PropDoc("/jfreechart-types.xml", out).run();
     }
 
-    private void printBean(ChartBeanInfo cbi, String typeName) {
-        out.println("<tr><td colspan=\"3\"><b>" + typeName + "</b></td></tr>");
+    private void printLi(String anchor, String text) {
+        out.println("<li><a href=\"#" + anchor + "\">" + text + "</a></li>");
+    }
+    private void printMajorAnchor(String anchor, String text) {
+        if (isWiki) out.println("= " + text + " =");
+        else out.println("<a name=\"" + anchor + "\"></a><h1>" + text + "</h1>");
+    }
+    
+    private void printMinorAnchor(String anchor, String text) {
+        if (isWiki) out.println("== " + text + " ==");
+        else out.println("<a name=\"" + anchor + "\"></a><h2>" + text + "</h2>"
+                + "\n &nbsp;&nbsp;&nbsp;<a href=\"\">top</a>"
+                );
+    }
+
+    private String javadocLink(ChartBeanInfo cbi) {
+        String cname = cbi.getClassname();
+        String path = cname.replace('.', '/') + ".html";
+        return "<a href=\"http://www.jfree.org/jfreechart/api/javadoc/" + path
+        + "\">" + cname + "</a>";
+    }
+    private void printBean(ChartBeanInfo cbi) {
+        out.println("<table>\n<tr><th>Name</th><th>Type</th><th>Default</th></tr>");
+        out.println("<tr><td colspan=\"3\"><b>" + javadocLink(cbi) + "</b></td></tr>");
         for (PropInfo pi : cbi.getProps()) {
             String def = pi.getDefault();
             String type = pi.getType();
@@ -67,6 +184,7 @@ public class PropDoc {
             out.println("<tr><td>" + pi.getName() + "</td><td>" + trunc(type)
                     + "</td><td><code>" + def + "</code></td></tr>");
         }
+        out.println("</table>");
     }
     private static String trunc(String s) {
         String ret = s;
@@ -75,5 +193,20 @@ public class PropDoc {
             ret = s.substring(ind + 1);
         }
         return ret;
+    }
+    
+    private static final Map<String,String> AXIS_TYPES = new HashMap();
+    static {
+        AXIS_TYPES.put("number", "NumberAxis");
+        AXIS_TYPES.put("log", "LogarithmicAxis");
+        AXIS_TYPES.put("date", "DateAxis");
+        AXIS_TYPES.put("period", "PeriodAxis");
+    };
+    private static Map supportedPlots = new HashMap();
+    static {
+        supportedPlots.put("PiePlot", true);
+        supportedPlots.put("PiePlot3D", true);
+        supportedPlots.put("XYPlot", true);
+        supportedPlots.put("CategoryPlot", true);
     }
 }
